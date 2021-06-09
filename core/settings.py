9fb34aa92927d52b05e24d78c860041009e0c802
stgrail <zzmrlol@gmail.com>
2021-06-09 22:01:21 +0300

    Добавил комментарии для приложений

--- core/settings.py
+++ core/settings.py
@@ -24,13 +24,18 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    #  приложение Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #  стронние приложения
 
+
+    #  наши приложения
+    'web',
     'hrv',
 ]
 
