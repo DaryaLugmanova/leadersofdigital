a4a4357ab6f985a078938f56f7d659482ff10ff6
StGrail <zzmrlol@gmail.com>
2021-06-19 20:39:23 +0300

    Add hrv app, create model

--- apps/hrv/apps.py
+++ apps/hrv/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class HrvConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hrv'
+    name = 'apps.hrv'
--- apps/hrv/models.py
+++ apps/hrv/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class HRV(models.Model):
+    MALE = 'Мужской'
+    FEMALE = 'Женский'
+
+    SEX_CHOICES = [
+        (MALE, 'Мужской'),
+        (FEMALE, 'Женский'),
+    ]
+    age = models.PositiveIntegerField(verbose_name='Возраст')
+    sex = models.CharField(verbose_name='Пол', max_length=15, choices=SEX_CHOICES)
+    hrv_with_rules = models.BooleanField(verbose_name='Ритмограмма выполнена по правилам', default=False)
+    hrv_file = models.FileField(verbose_name='Файл с ритмограммой', upload_to='hrv/%Y/%m/%d/', default=None)
+
+    class Meta:
+        verbose_name = 'Данные ритмограммы'
+        verbose_name_plural = 'Ритмограммы'
