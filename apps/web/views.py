bce06f85510dcc6677944bbcb2eb4618f1184ab7
StGrail <zzmrlol@gmail.com>
2021-06-19 21:14:11 +0300

    Добавил текст про датчики и ритмограмму * удалил лишние файлы * добавил вью для index.html

--- apps/web/views.py
+++ apps/web/views.py
@@ -1,5 +1,14 @@
-from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from django.views.generic import FormView
+from django.contrib import messages
+
+from apps.hrv.forms import HRVUploadForm
 
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'web/index.html'
+    form_class = HRVUploadForm
+
+    def post(self, request, *args, **kwargs):
+        messages.success(request, 'Ритмограмма успешно сохранена.')
+        return redirect('index')
